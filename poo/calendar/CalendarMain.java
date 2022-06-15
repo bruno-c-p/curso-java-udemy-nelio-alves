d8d0a99518ba1e886602299a939b010cbf27e6a4
Bruno Cardozo <bruno.cardozo.pereira@gmail.com>
2022-06-15 20:13:10 -0300

    Removing unused var from CalendarMain.java

--- poo/calendar/CalendarMain.java
+++ poo/calendar/CalendarMain.java
@@ -1,22 +1,19 @@
 package poo.calendar;
 
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
 
 public class CalendarMain {
-    
-    public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    public static void main(String[] args) {
 
         Date d = Date.from(Instant.parse("2022-06-25T21:15:30Z"));
 
         Calendar calendario = Calendar.getInstance();
         calendario.setTime(d);
-        calendario.add(Calendar.HOUR_OF_DAY, 4); 
-        d = calendario.getTime(); 
+        calendario.add(Calendar.HOUR_OF_DAY, 4);
+        d = calendario.getTime();
 
         int minutes = calendario.get(Calendar.MINUTE);
         int month = 1 + calendario.get(Calendar.MONTH);
